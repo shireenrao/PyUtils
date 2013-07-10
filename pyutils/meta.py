3f4cf00c1f7acd74e8586a5165f7eb5d161071d0
shireenrao <shireenrao@gmail.com>
2013-07-09 20:53:40 -0400

    Add meta.py

---
+++ pyutils/meta.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from hachoir_core.error import HachoirError
+from hachoir_core.cmd_line import unicodeFilename
+from hachoir_parser import createParser
+from hachoir_core.tools import makePrintable
+from hachoir_metadata import extractMetadata
+from hachoir_core.i18n import getTerminalCharset
+from sys import argv, stderr, exit
+
+if len(argv) != 2:
+    print >>stderr, "usage: %s filename" % argv[0]
+    exit(1)
+filename = argv[1]
+filename, realname = unicodeFilename(filename), filename
+parser = createParser(filename, realname)
+if not parser:
+    print >>stderr, "Unable to parse file"
+    exit(1)
+try:
+    metadata = extractMetadata(parser)
+except HachoirError, err:
+    print "Metadata extraction error: %s" % unicode(err)
+    metadata = None
+if not metadata:
+    print "Unable to extract metadata"
+    exit(1)
+
+#text = metadata.exportPlaintext()
+#charset = getTerminalCharset()
+#for line in text:
+#    print makePrintable(line, charset)
+#print type(metadata)
+#print dir(metadata)
+metaitems = metadata.getItems('creation_date')
+print metaitems.description + ' - ' + metaitems.values[0].text
+
+#for data in sorted(metadata):
+#	if not data.values:
+#		continue
+#	print "Description: " + data.description
+#	print "key: " + data.key
+#	for item in data.values:
+#		print item.text
+		 
+#print dir(metaitmes)
+
